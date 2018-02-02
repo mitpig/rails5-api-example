ff08cd3ff57164d8bafc32abf177981ec903281f
Patricio Tapia <native.devil@gmail.com>
2018-02-02 20:26:47 -0300

    REFACT: User Controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::UsersController < ApplicationController
-
+  before_action :set_user, only: [:login]
 
   def login
-    render :json, status: :unauthorized
+    if @user
+      render json: { status:true }, status: :ok
+    else
+      render json: { status:false }, status: :unauthorized
+    end
   end
 
   private
+  
   def set_user
     @user = User.find_by_email(params[:email])
     # Returns User if it exists, else nil
