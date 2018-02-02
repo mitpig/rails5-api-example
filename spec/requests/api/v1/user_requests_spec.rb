1648e87b54732e0fbbfe4691b34745d17b9785a9
Patricio Tapia <native.devil@gmail.com>
2018-02-02 20:34:37 -0300

    REFACT: User request specs

--- spec/requests/api/v1/user_requests_spec.rb
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "User requests", type: :request do s
 
-  context "With valid Password" do
+  context "with valid password" do
+    # Valid user params on DB.
+    let(:valid_params) {
+      "email" => "asd",
+      "password" => "asd"
+    }
 
     describe "POST /api/v1/login" do
       before { post "#{ @base_url }/login" }
-      it "Returns authorized status" do
+      it "returns authorized status" do
         expect(response).to have_http_status(:ok)
       end
     end
