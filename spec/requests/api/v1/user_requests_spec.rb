66826aabead1b42ff5eb10885cc504f48c8a4ba4
Patricio Tapia <native.devil@gmail.com>
2018-02-02 16:40:20 -0300

    FIX: User specs

--- spec/requests/api/v1/user_requests_spec.rb
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "User requests", type: :request do
 
   context "With valid Password" do
-    describe "/api/v1/login" do
-      post "/"
+    describe "POST /api/v1/login" do
+      before { post "/" }
       expect(response).to have_http_status(:error)
     end
   end
