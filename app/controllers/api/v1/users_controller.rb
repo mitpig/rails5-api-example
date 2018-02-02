aa14973deacfec80b007d12394db42adfd3b6c5d
Patricio Tapia <native.devil@gmail.com>
2018-02-02 16:57:27 -0300

    ADD: Basic auth

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::UsersController < ApplicationController
+
+
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password] && @user.email == params[:email] do
+      render :json, status: :ok
+    else
+      render :json, status: :unauthorized
+    end
+  end
+
 end
--- spec/requests/api/v1/user_requests_spec.rb
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe "User requests", type: :request do
   context "With valid Password" do
 
     describe "POST /api/v1/login" do
-
       before { post "/" }
-      it "Returns unauthorized status" do
-        expect(response).to have_http_status(:unauthorized)
+      it "Returns authorized status" do
+        expect(response).to have_http_status(:ok)
       end
-
     end
 
   end
