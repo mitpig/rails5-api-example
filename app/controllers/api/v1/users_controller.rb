65bd76bf6ea93e417f8492453cd0e6688e413238
Patricio Tapia <native.devil@gmail.com>
2018-02-02 17:00:58 -0300

    FIX: If blick

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
 
   def login
     @user = User.find_by_email(params[:email])
-    if @user.password == params[:password] && @user.email == params[:email] do
+    if @user.password == params[:password] && @user.email == params[:email]
       render :json, status: :ok
     else
       render :json, status: :unauthorized
--- spec/requests/api/v1/user_requests_spec.rb
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "User requests", type: :request do
   context "With valid Password" do
 
     describe "POST /api/v1/login" do
-      before { post "/" }
+      before { post "#{ @base_url }/login" }
       it "Returns authorized status" do
         expect(response).to have_http_status(:ok)
       end
