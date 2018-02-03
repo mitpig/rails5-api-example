0075935278aa80e27f06e3e8875b8950cdf53b3e
Patricio Tapia <native.devil@gmail.com>
2018-02-03 11:26:01 -0300

    REFACT: Authentication controller

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -8,8 +8,11 @@ class AuthenticateUser prepend SimpleCommand
     JsonWebToken.encode(user_id: user.id) if user
   end
 
-  private attr_accessor :email, :password
-  def user user = User.find_by_email_and_password(email, password)
+  private
+  attr_accessor :email, :password
+  
+  def user
+    user = User.find_by_email_and_password(email, password)
     return user if user
     errors.add :user_authentication, 'invalid credentials' nil
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'api/v1/login', to: 'authentication#authenticate'
+
   namespace :api do
     namespace :v1 do
-      post 'login', to: 'users#login'
       # User CRUD
       post 'new', to: 'users#create'
     end
