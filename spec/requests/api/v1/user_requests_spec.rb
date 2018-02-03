75654c0f4b5bd1651ff00077dfd34d195c3f0fd1
Patricio Tapia <native.devil@gmail.com>
2018-02-02 21:29:59 -0300

    ADD: Non valid credentials spec

--- spec/requests/api/v1/user_requests_spec.rb
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -2,24 +2,43 @@ require 'rails_helper'
 
 RSpec.describe "User requests", type: :request do
   let(:json_response) { JSON.parse(response.body) }
-  let(:valid_user_params){
-    {
-      "email" => "ptapscott0@spiegel.de",
-      "password" => "vFStrrnk4s"
-    }
-  }
 
   context "with valid password" do
+    let(:valid_user_params){
+      {
+        "email" => "ptapscott0@spiegel.de",
+        "password" => "vFStrrnk4s"
+      }
+    }
 
     describe "POST /api/v1/login" do
-
       before do
         post "#{ @base_url }/login",
         params: valid_user_params, as: :json
       end
-
       it "returns authorized status" do
         expect(response).to have_http_status(:ok)
+      end
+    end
+
+  end
+
+  context "with non valid password" do
+    let(:invalid_user_params){
+      {
+        "email" => "testing@spiegel.de",
+        "password" => "vFStrrnk4s"
+      }
+    }
+
+    describe "POST /api/v1/login" do
+      before do
+        post "#{ @base_url }/login",
+        params: invalid_user_params,
+        as: :json
+      end
+      it "returns unauthorized status" do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
 
