77e337901cf0c93a967b4031ea77b922d1b05d3d
Patricio Tapia <native.devil@gmail.com>
2018-02-02 21:16:40 -0300

    REFACT: ActiveRecord config on rails_helper for rspec

--- spec/requests/api/v1/user_requests_spec.rb
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "User requests", type: :request do
+  let(:json_response) { JSON.parse(response.body) }
   let(:valid_user_params){
     {
       "username" => "pepe",
@@ -11,12 +12,14 @@ RSpec.describe "User requests", type: :request do
   context "with valid password" do
 
     describe "POST /api/v1/login" do
+
       before do
         post "#{ @base_url }/login",
-        params: valid_user_params,
-        as: :json
+        params: valid_user_params, as: :json
       end
+
       it "returns authorized status" do
+        byebug
         expect(response).to have_http_status(:ok)
       end
     end
