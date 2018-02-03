2778980cd17e5948eab2848611c9b212f0a96ff8
Patricio Tapia <native.devil@gmail.com>
2018-02-03 09:14:30 -0300

    ADD: Create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,12 +9,26 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save?
+      render json: { status:true }, status: :ok
+    else
+      render json: { status:false }, status: :unauthorized
+    end
+  end
+
   private
 
   def set_user
     @user = User.find_by_email_and_password(params[:email], params[:password])
     # Returns User if it exists, else nil
     @user ? @user : nil
+  end
+
+  def user_params
+    params.require(:first_name, :last_name,
+      :email, , :password, :Username)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'login', to: 'users#login'
+      # User CRUD
+      post 'new', to: 'users#create'
     end
   end
 end
