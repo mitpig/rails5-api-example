11870a736a9cfb2d982584f9511d511346f279fa
Patricio Tapia <native.devil@gmail.com>
2018-02-04 16:32:16 -0300

    ADD: Uniqueness validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   self.table_name = "MOCK_DATA"
   self.primary_key = :id
   validates_presence_of :email, :first_name, :last_name, :Username
+  validates_uniqueness_of :email, :Username
 end
