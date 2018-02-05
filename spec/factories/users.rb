6a665e4fb2c24d8aca136da8237e785f89b13815
Patricio Tapia <native.devil@gmail.com>
2018-02-05 14:26:38 -0300

    ADD: User Factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    first_name {  }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.user_name(8) }
+    Username { Faker::Internet.password(8) }
   end
 end
