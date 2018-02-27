ca4f1e40e7c53039f034356e316213c7278f5d0c
Patricio Tapia <native.devil@gmail.com>
2018-02-26 23:35:36 -0300

    DELETE: Unnecesary code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  self.table_name = "MOCK_DATA"
-  self.primary_key = :id
   validates_presence_of :email, :first_name, :last_name, :username
   validates_uniqueness_of :email, :username
 end
