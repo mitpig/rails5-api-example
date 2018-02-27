045e57befdd86ad69c513faad2f0bbd3a1fcf708
Patricio Tapia <native.devil@gmail.com>
2018-02-27 00:03:19 -0300

    ADD: Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+unless Rails.env.production?
+  require 'factory_bot_rails'
+
+  loops = 5
+
+  # Create users
+  p "Creating #{ 'User'.pluralize(loops) }"
+
+  loops.times do
+    user = FactoryBot.build(:user)
+    if user.save
+      p "User: #{ user.username } created with password: #{ user.password }"
+    end
+  end
+
+end
